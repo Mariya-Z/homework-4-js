1284bc3606843dfb2127bd4a6a85e1bcb9be05b7
Mariya-Z <Mariya-Z@users.noreply.github.com>
2018-06-18 00:16:31 +0300

    hw 4

---
+++ js1.js
@@ -0,0 +1,81 @@
+function largest(){
+  let large = -Infinity;
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i] > large) {
+      large = arguments[i];
+    }
+  }
+  return large;
+}
+
+function smallest(){
+  let small = +Infinity;
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i] < small) {
+      small = arguments[i];
+    }
+  }
+  return small;
+}
+
+largest(2, 0.1, -5, 100, 3) // 100
+smallest(2, 0.1, -5, 100, 3) // -5
+
+function transform(baseArray){
+  let newArr = [];
+  newArr = baseArray.map(function(item){
+    return function(){
+      return item;
+    }
+  });
+ return newArr;
+}
+
+const baseArray = [10, 20, 30, 40, 50];
+const newArray = transform(baseArray);
+newArray[3](); // should return 40
+newArray[4](); // should return 50
+
+
+function sum(){
+  let result = 0;
+/*  let summary = [];
+  if (summary.length == 0){
+      for (let j = 0; j < arguments.length; ++j){
+      summary[j] = arguments[j];
+    }
+  }
+  if (summary.length > 1)
+  {
+    result += summary.pop() + sum(summary);
+  }*/
+
+  for (let i = 0; i < arguments.length; ++i){
+    result += arguments[i];
+  }
+  return result;
+}
+sum(1,3,5,7); //should return 16
+
+function countDown(maxNum){
+  setTimeout(function print(){
+    if (maxNum >= 0){
+      console.log(maxNum--);
+      setTimeout(print, 1000);
+    }
+  }, 1000);
+}
+
+countDown(3); // 3 2 1 0
+
+window.Function.prototype.myBind = function () {
+  const curr = this;
+  const cont = arguments[0];
+  const arrArgs = (Array.from(arguments)).slice(1);
+  return function() {
+    let fArgs = (Array.from(arguments));
+    return curr.apply(cont, arrArgs.concat(fArgs));
+  };
+}
+
+
